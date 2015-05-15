619d9483bd1f875e995ebaa0bbc40ef92e65a737
sphrcl <sphericalcommunications@gmail.com>
2015-05-15 13:30:54 -0400

    updates to footer

--- wp-content/themes/pacific beach mobile/includes/superfooter.php
+++ wp-content/themes/pacific beach mobile/includes/superfooter.php
@@ -1,32 +1,34 @@
 
 <div class="superfooter">
 	<div class="phone-carry">
-		<div class="phoneno">
-			<img src="<?php bloginfo ('template_url'); ?>/images/" />
-			<div class="numadd">
-				<p class="nummob">888.352.3650</p>
-				<a href="https://www.google.com/maps/place/Row+NYC/@40.7591338,-73.9883055,16z/data=!4m2!3m1!1s0x0:0x8890505e02666256" target="_blank"><p class="addmob">700 8TH AVENUE, <br>NEW YORK, NY 10036</p></a>
-			</div>
-		</div>
+		<ul>
+			<li><i class="fa fa-map-marker"></i><span>2490 Kalakaua Avenue Honolulu, Hawaii 96815</span></li>
+			<li><i class="fa fa-mobile"></i><span>808-922-1233</span></li>
+			<li><i class="fa fa-user"></i><span>Contact Us</span></li>
+		</ul>
 	</div>
 	
 	<div class="contact-social">
 		<div class="contacthere">
+			
+			<p><a href="#">Visit desktop site</a></p>
+			<!--
 			<ul>
 				<li class="li-mob"><a href="#"><span class="fa fa-mobile"></span></a></li>
 				<li class="li-env"><a href="mailto:info@rownyc.com"><span class="fa fa-envelope"></span></a></li>
 				<li class="li-map"><a href="https://www.google.com/maps/place/Row+NYC/@40.7591338,-73.9883055,16z/data=!4m2!3m1!1s0x0:0x8890505e02666256" target="_blank"><span class="fa fa-map-marker"></span></a></li>
 			</ul>
+			-->
+
 		</div>
 		<div class="socialhere">
 			<ul>
-				<li><a href="http://www.facebook.com/RowNYC" target="_blank"><span class="fa fa-facebook"></span></a></li>
-				<li><a href="http://twitter.com/rownychotel" target="_blank"><span class="fa fa-twitter"></span></a></li>
-				<li><a href="http://www.youtube.com/RowNYC" target="_blank"><span class="fa fa-youtube-play"></span></a></li>
-				<li class="last"><a href="http://www.instagram.com/rownyc" target="_blank"><span class="fa fa-instagram"></span></a></li>
+				<li><a href="#" target="_blank"><span class="fa fa-facebook"></span></a></li>
+				<li><a href="#" target="_blank"><span class="fa fa-twitter"></span></a></li>
+				<li><a href="#" target="_blank"><span class="fa fa-youtube-play"></span></a></li>
+				<li><a href="#" target="_blank"><span class="fa fa-google-plus"></span></a></li>
+				<li class="last"><a href="#" target="_blank"><span class="fa fa-instagram"></span></a></li>
 			</ul>
 		</div>
 	</div>
 </div>
-
-<br>
