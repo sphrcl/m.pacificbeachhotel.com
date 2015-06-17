cfaa7a56caf8fd87d431a280adf847f3da4a202b
sphrcl <sphericalcommunications@gmail.com>
2015-06-17 15:34:55 -0400

    small fix

--- wp-content/themes/pacific beach mobile/index.php
+++ wp-content/themes/pacific beach mobile/index.php
@@ -16,7 +16,7 @@ get_header(); ?>
 			<div class="swiper-container swiper1">
 				<div class="swiper-wrapper">
 					<div class="swiper-slide">
-						<div class="slidimge" style="background: url('http://sphericalcommunications.com/pb-mobile/wp-content/uploads/2015/05/pacific-beach-resort-ocean-front-this.jpg')"></div>
+						<div class="slidimge" style="background: url('<?php bloginfo ('template_url'); ?>/images/pacific-beach-hotel-slider.jpg')"></div>
 						<p>
 							<span class="mainbold">Pacific Beach Hotel</span>
 							<span class="textthin">Beautiful Oceanfront</span>
@@ -37,7 +37,7 @@ get_header(); ?>
 						<div class="slidimge" style="background-image: url('<?php bloginfo ('template_url'); ?>/images/pacific-beach-hotel-water-surfing.jpg');"></div>
 						<p>
 							<span class="mainbold">Pacific Beach Hotel</span>
-							<span class="textthin">Just Steps from Waikiki Beach</span>
+							<span class="textthin">Subheading subheading</span>
 						</p>
 						<a class="book-now" href="http://pacificbeachhotel.reztripmobile.com">Book Now</a>
 					</div>					
@@ -69,7 +69,29 @@ get_header(); ?>
 			
 			<?php endwhile; endif; wp_reset_query(); ?>	
 		</div>
-		
+
+		<div id="st-accordion" class="st-accordion">
+
+			<ul class="linkers" style="margin-bottom: 0;">
+				<li>
+					<a style="padding: 0px; height: 58px; line-height: 58px;" href="#">PACIFIC BEACH HOTEL</a>
+
+					<div class="st-content">
+
+						<div class="st-content-container">
+							
+							<p>Graceful waves roll in on legendary Waikiki Beach just steps from one of the most ideally located oceanfront hotels in Honolulu. Unwind in Pacific Beach Hotel’s comfortable, family-friendly guestrooms offering sweeping beachfront views. Visit the hotel’s 280,000 gallon, indoor Oceanarium, the only one of its kind. Explore the rich beauty of the hotel’s surroundings; brimming with endless adventure. Pacific Beach Hotel invites you to make memories that will last a lifetime in the place where Aloha lives.</p>
+
+						</div>
+		                
+		            </div>
+
+		            <div class="content-opener"><i class="fa fa-plus"></i></div>
+
+				</li>
+			</ul>
+
+		</div>		
 		
 		<?php query_posts('post_type=page&p=13'); if(have_posts()) : while(have_posts()) : the_post(); $imgsrc = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "Full"); ?>
 		<div class="fullybox shadow" style="background: url('<?php echo $imgsrc[0]; ?>')">
@@ -79,7 +101,7 @@ get_header(); ?>
 		<?php endwhile; endif; wp_reset_query(); ?>	
 		<div class="fullybox shadow" style="background: url('http://sphericalcommunications.com/pb-mobile/wp-content/uploads/2015/05/pacific-beach-cceanarium-tank.jpg')">
 			<p class="fullytext">Dining</p>
-				<a class="coverlink" href="http://sphericalcommunications.com/pb-mobile/dining/"></a>
+				<a class="coverlink" href="/dining/"></a>
 		</div>
 		<?php include(TEMPLATEPATH . '/includes/superfooter.php'); ?>
 		
