83288d715308c79996a2121b363730073ba313be
jeromemisfit <jerome@misfit-inc.com>
2016-12-12 14:40:48 +0800

    cleanup and add save the children css style

--- wp-content/themes/pacific-beach-mobile/includes/superfooter.php
+++ wp-content/themes/pacific-beach-mobile/includes/superfooter.php
@@ -1,13 +1,70 @@
 
 <div class="superfooter">
+	
+	<ul class="superfooter-info">
+		<li>
+			<i class="fa fa-map-marker"></i>
+			<a href="https://www.google.com/maps/place/2490+Kalakaua+Ave,+Honolulu,+HI+96815/@21.2746857,-157.8232682,17z/data=!3m1!4b1!4m2!3m1!1s0x7c0072773c7ca3f1:0x73d8eb6aae42e3f2">Pacific Beach Hotel</a>
+			<div class="clear"></div>
+		</li>
+		<li>
+			<i class="fa fa-mobile"></i>
+			<a href="tel:1808-922-1233">808 922 1233</a>
+			<div class="clear"></div>
+		</li>
+		<li>
+			<i class="fa fa-user"></i>
+			<a href="http://m.pacificbeachhotel.com/contact-us/">Contact Us</a>
+			<div class="clear"></div>
+		</li>
+	</ul>
+
+	<div class="superfooter-visit-socials">
+		<div class="superfooter-visit">
+			<a href="http://www.pacificbeachhotel.com/?isfullhtml=y">Visit desktop site</a>
+		</div>
+
+		<ul class="superfooter-socials">
+			<li><a target="_blank" href="https://www.facebook.com/PacificBeachHotel"><i class="fa fa-facebook"></i></a></li>
+			<li><a target="_blank" href="https://twitter.com/PacificBeachHtl"><i class="fa fa-twitter"></i></a></li>
+			<li><a target="_blank" href="https://plus.google.com/106176366593354536658/posts?hl=en"><i class="fa fa-google-plus"></i></a></li>
+			<li><a target="_blank" href="http://www.tripadvisor.com/Hotel_Review-g60982-d87111-Reviews-Pacific_Beach_Hotel-Honolulu_Oahu_Hawaii.html"><i class="fa-tripadvisor"></i></a></li>
+			<li><a target="_blank" href="https://instagram.com/pacificbeachhotel/"><i class="fa fa-instagram"></i></a></li>
+		</ul>
+	</div>
+
+	<div class="superfooter-highgate">
+		<a href="http://www.highgate.com/" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/highgate-logo.png" alt="Highgate" /></a>
+	</div>
+
+	<div class="superfooter-ta">
+		<p>TA-114-954-4448-01</p>
+	</div>
+
+	<div class="superfooter-save">
+		<div class="superfooter-save-img">
+			<a target="_blank" href="https://goo.gl/OJexpZ">
+				<img src="<?php bloginfo('template_url'); ?>/images/save-the-children-logo.png" alt="Save the Children" />
+			</a>
+		</div>
+
+		<p>Pacific Beach Hotel is a proud supporter of <a target="_blank" href="https://goo.gl/OJexpZ">Save the Children</a>, internationally recognized for giving children a healthy start, the opportunity to learn and protection from harm. To donate and make a difference in a child’s life, please click on the link below. 100% of your donation benefits Save the Children. Thank you for your support.</p>
+
+		<div class="superfooter-save-donate">
+			<a target="_blank" href="https://goo.gl/OJexpZ">DONATE NOW</a>
+		</div>
+	</div>
+
+</div>
+
+<div class="superfooter" style="display: none;">
 	<div class="phone-carry">
 		<ul>
 			<li class="address">
 				<i class="fa fa-map-marker"></i>
-			  	<span><a href="https://www.google.com/maps/place/2490+Kalakaua+Ave,+Honolulu,+HI+96815/@21.2746857,-157.8232682,17z/data=!3m1!4b1!4m2!3m1!1s0x7c0072773c7ca3f1:0x73d8eb6aae42e3f2">Pacific Beach Hotel</a></span>
-			  	<br />
-				<!--<span class="position"><a href="https://www.google.com/maps/place/2490+Kalakaua+Ave,+Honolulu,+HI+96815/@21.2746857,-157.8232682,17z/data=!3m1!4b1!4m2!3m1!1s0x7c0072773c7ca3f1:0x73d8eb6aae42e3f2">Honolulu, Hawaii 96815</a></span>
-			--></li>
+				<span><a href="https://www.google.com/maps/place/2490+Kalakaua+Ave,+Honolulu,+HI+96815/@21.2746857,-157.8232682,17z/data=!3m1!4b1!4m2!3m1!1s0x7c0072773c7ca3f1:0x73d8eb6aae42e3f2">Pacific Beach Hotel</a></span>
+				<br />
+			</li>
 			<li class="phone">
 				<i class="fa fa-mobile"></i>
 				<span><a href="tel:1808-922-1233">808 922 1233</a></span>
@@ -40,17 +97,6 @@
 		<div class="ta-number">
 			<p>TA-114-954-4448-01</p>
 		</div>
-
-		<!--div class="save-the-children">
-				
-				
-				<a target="_blank" href="https://secure.savethechildren.org/site/c.8rKLIXMGIpI4E/b.6239401/k.C01C/Global_Action_Fund/apps/ka/sd/donor.asp?msource=cpkhhgaf1214&utm_source=Highgate2014&utm_medium=link&utm_campaign=highgatehotels1214"><img width="270px" src="http://m.pacificbeachhotel.com/wp-content/uploads/2015/08/save-the-children-logo.png"></a>
-					
-				<p class="save">Join us in supporting Save the Children, internationally recognized for giving children a healthy start, the opportunity to learn and protection from harm.100% of your donation benefits Save the Children. Thank you for your support.</p>
-
-				<a class="button" target="_blank" href="https://secure.savethechildren.org/site/c.8rKLIXMGIpI4E/b.6239401/k.C01C/Global_Action_Fund/apps/ka/sd/donor.asp?msource=cpkhhgaf1214&utm_source=Highgate2014&utm_medium=link&utm_campaign=highgatehotels1214">donate now</a>
-			
-		</div-->	
 	
 	</div>
 </div>
